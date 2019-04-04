3c09897717edd2e3970b829b185cd6516e9afc4f
Nicolae Teslaru <teslaruzero@gmail.com>
2019-04-04 16:36:09 +0200

    Add Tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_133842) do
+ActiveRecord::Schema.define(version: 2019_04_04_143254) do
 
   create_table "articles", force: :cascade do |t|
     t.string "url"
     t.string "title"
     t.text "description"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
